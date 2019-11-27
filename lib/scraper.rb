7047d6091fc349e13100f649a6c533bbbb70e89e
Jesse Gonzalez <jessegonzalez024@gmail.com>
2019-11-27 07:03:44 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ html = open("https://flatironschool.com/")
 
 doc = Nokogiri::HTML(html)
 
-courses = doc.css().text
+courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b.inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC").text
 
 courses.each do |course|
   puts course.text.strip
